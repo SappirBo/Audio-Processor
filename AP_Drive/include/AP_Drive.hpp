7cb09e7bd5f6db3c75c2a638a2ddc4a935ab2f0b
SappirBo <sappirbohbot@gmail.com>
2024-04-18 15:26:14 +0300

    Add comments and merge with remote

--- AP_Drive/include/AP_Drive.hpp
+++ AP_Drive/include/AP_Drive.hpp
@@ -6,17 +6,22 @@
 
 class AP_Drive {
 public:
+    /** AP_Drive Constructor */
     AP_Drive(AudioIO& audio,int16_t level = 0, int16_t drive = 0);
 
+    /** Process and apply the settings on the audio */
     void processAudio();
+
+    /** Set the Drive*/
+    void setDrive(int16_t drive);
+
+    /** Set the Levels*/
+    void setLevel(int16_t level);
 
 private:
     AudioIO& m_audio;
     float m_drive;
     int16_t m_level;
-    
-    void setDrive(int16_t drive);
-    void setLevel(int16_t level);
 
     int16_t applyDrive(int16_t sample);
     int16_t applyLevel(int16_t sample);
