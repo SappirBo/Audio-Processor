fb2c23e1f436e9c4d2a0b97135885bdbb42e677d
SappirBo <sappirbohbot@gmail.com>
2024-04-27 20:11:06 +0300

    apply 'process audio'

--- AP_Compressor/include/AP_Compressor.hpp
+++ AP_Compressor/include/AP_Compressor.hpp
@@ -6,10 +6,11 @@
 class AP_Compressor: public AP_Audio_Effect
 {
 private:
-    float m_threshold;
+    /* m_threashold is set to be number from 0 to -100 ([0,-100]) as 0 in max amplitude*/
+    float m_threshold; 
     float m_ratio;
-    float m_attack;
-    float m_release;
+    float m_attack;  //ms
+    float m_release; //ms
 public:
     AP_Compressor(AP_AudioIO& _audio, int32_t _mix = 0, int32_t _level = 0, 
         float _threshold = 0, float _ratio = 1.0, float _attack = 1.0, float _release = 100.0);
@@ -20,5 +21,8 @@ public:
     void setRatio(float _r);
     void setAttack(float _a);
     void setRelease(float _r);
+
+private:
+    void compress(int16_t& _sample);
 };
 #endif
--- AP_Compressor/src/AP_Compressor.cpp
+++ AP_Compressor/src/AP_Compressor.cpp
@@ -31,3 +31,48 @@ void AP_Compressor::setRelease(float _r)
 {
     m_release = _r;
 }
+
+void AP_Compressor::processAudio()
+{
+    int32_t sample_rate = m_audio.getSampleRate();
+    float one_second{1000.0};
+    float attack_in_seconds{static_cast<float>(m_attack)/one_second};
+    float release_in_seconds{static_cast<float>(m_release)/one_second};
+    
+    int32_t bits_to_attack {static_cast<int32_t>(static_cast<float>(sample_rate) * attack_in_seconds) };
+    int32_t bits_to_release{static_cast<int32_t>(static_cast<float>(sample_rate) * release_in_seconds)};
+
+    int32_t threshold_range{100};
+    int32_t sample_threashold_chunck{INT16_MAX/threshold_range};
+    int32_t sample_threashold{static_cast<int32_t>(m_threshold * static_cast<float>(sample_threashold_chunck))};
+
+    bool apply_compression{false};
+    int32_t attack_counter{0};
+    int32_t release_counter{0};
+
+    for(auto& sample: m_audio.getSamples())
+    {
+        if(sample > sample_threashold)
+        {
+            apply_compression = true;
+            release_counter = 0;
+        }
+
+        if (apply_compression)
+        {
+            if(attack_counter < bits_to_attack)
+            {
+                attack_counter++;
+            }
+            else if(release_counter <= bits_to_release)
+            {
+                compress(sample);
+                release_counter++;
+            }
+            else
+            {
+                apply_compression = false;
+            }
+        }
+    }
+}
