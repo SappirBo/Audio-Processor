5c96a3d60fc3978a4d0c770ca54a66a562bd5405
Sappir Bohbot <sappirbohbot@gmail.com>
2024-05-16 10:35:58 +0300

    adding process audio function

--- AP_Equalizer/include/AP_Equalizer.hpp
+++ AP_Equalizer/include/AP_Equalizer.hpp
@@ -22,27 +22,44 @@ class AP_Equalizer: public AP_Audio_Effect
         int16_t m_Qfactor;
         EqualizerType m_eqType;
     public:
-        AP_Equalizer_Band(float _gain, int16_t _frequency, int16_t _Qfactor, EqualizerType _eq_type):
-        m_gain(_gain), m_frequency(_frequency), m_Qfactor(_Qfactor), m_eqType(_eq_type){}
+        AP_Equalizer_Band(float gain, int16_t frequency, int16_t Qfactor, EqualizerType eq_type):
+        m_gain(gain), m_frequency(frequency), m_Qfactor(Qfactor), m_eqType(eq_type){}
         ~AP_Equalizer_Band() = default;
     };
 private:
     std::vector<AP_Equalizer_Band> m_eq_band_list;
 public:
-    AP_Equalizer(AP_AudioIO& _audio, int32_t _mix = 0, int32_t _level = 0);
+    AP_Equalizer(AP_AudioIO& audio, int32_t mix = 0, int32_t level = 0);
     ~AP_Equalizer();
 
     void processAudio();
 
-    bool addBand(float _gain, int16_t _frequency, int16_t _Qfactor, EqualizerType _eq_type);
+    void addBand(float gain, int16_t frequency, int16_t Qfactor, EqualizerType eq_type);
 
-    bool applyEqBand(const AP_Equalizer_Band& band);
+    void applyEqBand(const AP_Equalizer_Band& band);
 };
 
-// AP_Equalizer::AP_Equalizer(/* args */)
-// {
-// }
+AP_Equalizer::AP_Equalizer(AP_AudioIO& audio, int32_t mix, int32_t level):
+AP_Audio_Effect(audio,mix,level){}
 
-// AP_Equalizer::~AP_Equalizer()
-// {
-// }
+AP_Equalizer::~AP_Equalizer(){}
+
+void AP_Equalizer::addBand(float gain, int16_t frequency, int16_t Qfactor, EqualizerType eq_type)
+{
+    AP_Equalizer_Band new_band{gain, frequency, Qfactor, eq_type};
+    m_eq_band_list.push_back(new_band);
+}
+
+void AP_Equalizer::processAudio()
+{
+    for(auto& band: m_eq_band_list)
+    {
+        applyEqBand(band);
+    }
+}
+
+void AP_Equalizer::applyEqBand(const AP_Equalizer_Band& band)
+{
+    int32_t N{1024};
+    int32_t sample_rate{m_audio.getSampleRate()};
+}
